ec560ead6366f5502e308f33ff9ace24e50c84ce
Miguel-AIProgression <miguel@aiprogression.nl>
2026-05-08 20:17:05 +0200

    refactor(maatwerk): runtime-queries naar Module (stap 4/10)

--- frontend/src/modules/maatwerk/index.ts
+++ frontend/src/modules/maatwerk/index.ts
@@ -2,3 +2,38 @@
 export { berekenPrijsOppervlakM2, berekenOmtrekMeter } from './lib/oppervlak'
 export { berekenMaatwerkPrijs } from './lib/prijs'
 export { berekenMaatwerkAfleverdatumViaSeam } from './lib/leverdatum'
+
+export {
+  fetchVormen,
+  fetchAfwerkingTypes,
+  fetchTypeBewerkingen,
+  fetchStandaardAfwerking,
+  fetchAfwerkingVoorKleur,
+  fetchAlleStandaardAfwerkingen,
+  fetchMaatwerkKwaliteiten,
+  fetchMaatwerkKleurenVoorKwaliteit,
+  fetchMaatwerkKleurOptiesVoorKwaliteit,
+  fetchMaatwerkKwaliteitOpties,
+  fetchKoppelingenVoorKleurLabel,
+  fetchKwaliteiten,
+  fetchMaatwerkArtikelNr,
+  fetchStandaardBandKleur,
+  fetchBandDefaultsVoorKwaliteit,
+  fetchKwaliteitM2Prijs,
+  searchKwaliteitenViaProducten,
+  fetchKleurenVoorKwaliteit,
+  fetchStandaardMatenVoorKwaliteit,
+  fetchMaatwerkLevertijdHint,
+} from './queries/maatwerk-runtime'
+
+export type {
+  MaatwerkVormRow,
+  AfwerkingTypeRow,
+  BandLabelKoppeling,
+  BandDefault,
+  BandDefaultRow,
+  KwaliteitOptie,
+  KleurOptie,
+  StandaardMaat,
+  MaatwerkLevertijdHintResult,
+} from './queries/maatwerk-runtime'
