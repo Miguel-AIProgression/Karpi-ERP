c58a4b062eb2fcca3624f5023c3c97fc2446ffa1
Miguel-AIProgression <miguel@aiprogression.nl>
2026-06-11 21:34:02 +0200

    feat(logistiek): cron verhoek-send-elke-minuut (mig 373)

---
+++ supabase/migrations/373_verhoek_send_cron.sql
@@ -0,0 +1,29 @@
+-- Migratie 373: pg_cron schedule voor de verhoek-send edge function
+-- Plan: docs/superpowers/plans/2026-06-11-verhoek-transporteur-xml-sftp.md
+-- Spiegelt mig 173 (hst-send). Vault-secret 'cron_token' bestaat al.
+--
+-- Idempotent.
+
+DO $$
+BEGIN
+  BEGIN
+    PERFORM cron.unschedule('verhoek-send-elke-minuut');
+  EXCEPTION WHEN OTHERS THEN
+    NULL;
+  END;
+END $$;
+
+SELECT cron.schedule(
+  'verhoek-send-elke-minuut',
+  '* * * * *',
+  $$
+  SELECT net.http_post(
+    url := 'https://wqzeevfobwauxkalagtn.supabase.co/functions/v1/verhoek-send',
+    headers := jsonb_build_object(
+      'Authorization', 'Bearer ' || (SELECT decrypted_secret FROM vault.decrypted_secrets WHERE name = 'cron_token'),
+      'Content-Type', 'application/json'
+    ),
+    body := '{}'::jsonb
+  );
+  $$
+);
